1b6a5572064abc3667ae56ea32a08e6fcf6158b9
AlexandraBosquee <114166308+AlexandraBosquee@users.noreply.github.com>
2022-10-24 13:30:16 +0200

    essay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
-
-module.exports = {
-  theme: {
-    // ...
-  },
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: 'base', // only generate global styles
-      strategy: 'class', // only generate classes
-    }),
-  ],
-} 
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [  require("@tailwindcss/forms")({
+    strategy: 'base', // only generate global styles
+    strategy: 'class', // only generate classes
+  }),
+],
 
   
 }
+
+
